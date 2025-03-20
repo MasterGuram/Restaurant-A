233e286162d6c9ced2521ab476ab35e052612573
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-03-20 15:34:25 +0400

    add event listener on multiple elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,10 @@ window.addEventListener("load", function (){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
+
+// add event listener on multiple elements
+const addEventOnElements = function (elements, eventType, callback){
+    for (let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}

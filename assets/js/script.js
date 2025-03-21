c4a98deb94ac23897ff18690e020514f84e474fd
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-03-21 09:15:49 +0400

    Navbar toggle js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,16 @@ const addEventOnElements = function (elements, eventType, callback){
         elements[i].addEventListener(eventType, callback);
     }
 }
+
+// Navbar
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);

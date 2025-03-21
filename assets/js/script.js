c89ec6c703816527981b865d06a103edd80f4e7e
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-03-21 09:29:39 +0400

    Header js

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,28 @@ const toggleNavbar = function () {
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
+
+// Header
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+    const isScrollBottom = lastScrollPos < window.scrollY;
+    if (isScrollBottom) {
+        header.classList.add("hide");
+    } else {
+        header.classList.remove("hide");
+    }
+
+    lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
+        header.classList.add("active");
+        hideHeader();
+    } else {
+        header.classList.remove("active");
+    }
+})
